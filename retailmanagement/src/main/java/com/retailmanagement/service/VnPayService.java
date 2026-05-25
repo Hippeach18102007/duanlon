be0ad2e802ba82a38aed2f96a4dec6407b7d2bc6
Dao Duc <daod1068@gmail.com>
2026-05-25 10:02:07 +0700

    test time

--- retailmanagement/src/main/java/com/retailmanagement/service/VnPayService.java
+++ retailmanagement/src/main/java/com/retailmanagement/service/VnPayService.java
@@ -38,6 +38,7 @@ public class VnPayService {
 
         Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        formatter.setTimeZone(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         vnp_Params.put("vnp_CreateDate", formatter.format(cld.getTime()));
 
         cld.add(Calendar.MINUTE, 15);
